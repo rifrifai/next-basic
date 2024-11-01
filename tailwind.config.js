cdf3fd0bc7c9b2b7bb3186164c3ffd76a29f61e1
R. Abdurrohman <rifaiabdurrohman8@gmail.com>
2024-11-01 17:13:22 +0700

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,8 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["corporate"],
   },
-  plugins: [],
 };
